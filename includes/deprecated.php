fe50a17f6f8e301d434a3703ba5c4c7cdf0eadda
Simon Prosser <simon@pagelines.com>
2012-10-05 20:18:53 +0100

    fix plv8
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- includes/deprecated.php
+++ includes/deprecated.php
@@ -78,3 +78,4 @@ define( 'CORE_VERSION', get_theme_mod( 'pagelines_version' ) );
 define( 'THEMENAME', 'PageLines' );
 define( 'CHILD_URL', get_stylesheet_directory_uri() );
 define( 'CHILD_IMAGES', CHILD_URL . '/images' );
+define( 'CHILD_DIR', get_stylesheet_directory() );
