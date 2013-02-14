0e0ea6ab775a65f326dc15c40dc6a120ac507be5
Simon Prosser <simon@pagelines.com>
2013-02-14 22:03:12 +0000

    V3 Update callout
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- sections/callout/section.php
+++ sections/callout/section.php
@@ -18,96 +18,83 @@
  */
 class PageLinesCallout extends PageLinesSection {
 
-	var $tabID = 'callout_meta';
+	function section_opts(){
+		$opts = array(
+			 		array(
+						'type' 			=> 'multi',
+						'title' 		=> 'Enter text for your callout banner section',
+						'opts'			=> array(
 
+					array(
+						'key'			=> 'pagelines_callout_header',
+						'type' 			=> 'text',
+						'size'			=> 'big',
+						'label' 		=> 'Callout Header'),
 
-	/**
-	*
-	* @TODO document
-	*
-	*/
-	function section_optionator( $settings ){
-		$settings = wp_parse_args($settings, $this->optionator_default);
-		
-			$page_metatab_array = array(
-				'pagelines_callout_text' => array(
-						'type' 				=> 'text_multi',
-						'inputlabel' 		=> 'Enter text for your callout banner section',
-						'title' 			=> $this->name.' Text',	
-						'selectvalues'	=> array(
-							'pagelines_callout_header'		=> array('inputlabel'=>'Callout Header', 'default'=> ''),
-							'pagelines_callout_subheader'	=> array('inputlabel'=>'Callout Subtext', 'default'=> '')
-						),				
-						'shortexp' 			=> 'The text for the callout banner section',
-						'exp' 				=> 'This text will be used as the title/text for the callout section of the theme.'
+					array(
+						'key'			=> 'pagelines_callout_subheader',
+						'type' 			=> 'text',
+						'size'			=> 'big',
+						'label' 		=> 'Callout Subtext'),
 
-				),
-				'pagelines_callout_align' => array(
-					'type' 			=> 'select',
-					'inputlabel' 	=> 'Select Alignment',
-					'title' 		=> 'Callout Alignment',			
-					'shortexp' 		=> 'Aligns the action left or right (defaults right)',
-					'exp' 			=> 'Default alignment for the callout "action" is on the right.', 
-					'selectvalues'	=> array(
-						'right'		=> array('name'	=>'Align Right'),
-						'left'		=> array('name'	=>'Align Left'),
-						'center'	=> array('name'	=>'Align Center'),
-					),
-				),
-				'pagelines_callout_cta' => array(
-					'type'		=> 'multi_option', 
-					'title'		=> __('Callout Action Button', 'pagelines'), 
-					'shortexp'	=> __('Enter the options for the Callout button', 'pagelines'),
-					'selectvalues'	=> array(
-						'pagelines_callout_button_link' => array(
-							'type' => 'text',
-							'inputlabel' => 'Button link destination (URL - Required)',
+					array(
+			 			'type'			=> 'select',
+			 			'title'			=> 'Callout Alignment',
+			 			'key'			=> 'pagelines_callout_align',
+			 			'label'			=> 'Select alignment.',
+			 			'opts'			=> array(
+
+			 			'right'			=> array( 'name' => 'Right' ),
+			 			'left'			=> array( 'name' => 'Left' ),
+			 			'center'		=> array( 'name' => 'Center' )
+									)
+			 					)
+							)
 						),
-						'pagelines_callout_button_text' => array(
-							'type' 			=> 'text',
-							'inputlabel' 	=> 'Callout Button Text',					
-						),		
-						'pagelines_callout_button_target' => array(
-							'type'			=> 'check',
-							'default'		=> false,
-							'inputlabel'	=> 'Open link in new window.',
-						),
-						'pagelines_callout_button_theme' => array(
-							'type'			=> 'select',
-							'default'		=> false,
-							'inputlabel'	=> 'Select Button Color',
-							'selectvalues'	=> array(
-								'primary'	=> array('name' => 'Blue'), 
-								'warning'	=> array('name' => 'Orange'), 
-								'important'	=> array('name' => 'Red'), 
-								'success'	=> array('name' => 'Green'), 
-								'info'		=> array('name' => 'Light Blue'), 
-								'reverse'	=> array('name' => 'Grey'), 
-							),
-						),
-					),
-				),						
-				'pagelines_callout_image' => array(
-					'type' 			=> 'image_upload',
-					'imagepreview' 	=> '270',
-					'inputlabel' 	=> 'Upload custom image',
-					'title' 		=> $this->name.' Image',						
-					'shortexp' 		=> 'Input Full URL to your custom header or logo image.',
-					'exp' 			=> 'Overrides the button output with a custom image.'
-				),
-				
-			);
 
-			$metatab_settings = array(
-					'id' 		=> $this->tabID,
-					'name' 		=> 'Callout',
-					'icon' 		=> $this->icon, 
-					'clone_id'	=> $settings['clone_id'], 
-					'active'	=> $settings['active']
+			 		array(
+			 			'type'			=> 'multi',
+			 			'title'			=> 'Callout Action Button',
+			 			'opts'			=> array(
+
+			 		array(
+			 			'type'			=> 'text',
+			 			'key'			=> 'pagelines_callout_button_link',
+			 			'label'			=> 'Button link destination (URL - Required)' ),
+
+			 		array(
+			 			'key'			=> 'pagelines_callout_button_text',
+			 			'type'			=> 'text',
+			 			'label'			=> 'Callout Button Text'),
+
+			 		array(
+			 			'key'			=> 'pagelines_callout_button_target',
+			 			'type'			=> 'check',
+			 			'label'			=> 'Open link in new window'),
+
+			 		array(
+			 			'type'			=> 'select',
+			 			'title'			=> 'Button Theme',
+			 			'key'			=> 'pagelines_callout_button_theme',
+			 			'opts'			=> array(
+
+			 			'primary'		=> array('name' => 'Blue'),
+						'warning'		=> array('name' => 'Orange'),
+						'important'		=> array('name' => 'Red'),
+						'success'		=> array('name' => 'Green'),
+						'info'			=> array('name' => 'Light Blue'),
+						'reverse'		=> array('name' => 'Grey')
+			 						)
+			 					)
+							)
+			 			),
+			 		array(
+			 			'type'			=> 'image_upload',
+			 			'key'			=> 'pagelines_callout_image',
+			 			'title'			=> 'Callout Image'
+					)
 				);
-
-			register_metatab($metatab_settings, $page_metatab_array);
-
+		return $opts;
 	}
 
 	/**
@@ -122,60 +109,56 @@ class PageLinesCallout extends PageLinesSection {
 		$call_btext = $this->opt( 'pagelines_callout_button_text', $this->tset );
 		$call_btheme = $this->opt( 'pagelines_callout_button_theme', $this->tset );
 		$target = ( $this->opt( 'pagelines_callout_button_target', $this->oset ) ) ? 'target="_blank"' : '';
-		$call_action_text = ($this->opt('pagelines_callout_action_text', $this->oset)) ? $this->opt('pagelines_callout_action_text', $this->oset) : __('Start Here', 'pagelines');
+		$call_action_text = ($this->opt('pagelines_callout_action_text', $this->oset)) ? $this->opt( 'pagelines_callout_action_text', $this->oset ) : __( 'Start Here', 'pagelines' );
 
 		$styling_class = ($call_sub) ? 'with-callsub' : '';
-		
-		$alignment = $this->opt('pagelines_callout_align', $this->oset);
 
-		$call_align = ($alignment == 'left') ? '' : 'rtimg';	
+		$alignment = $this->opt( 'pagelines_callout_align', $this->oset );
 
-		if($call_title || $call_img){ ?>
-			
-<?php if($alignment == 'center'): ?>
+		$call_align = ( $alignment == 'left' ) ? '' : 'rtimg';
+
+		if( $call_title || $call_img ) {
+
+			if( $alignment == 'center' ): ?>
 <div class="callout-area fix callout-center <?php echo $styling_class;?>">
 	<div class="callout_text">
 		<div class="callout_text-pad">
-			<?php $this->draw_text($call_title, $call_sub, $call_img); ?>
+			<?php $this->draw_text( $call_title, $call_sub, $call_img ); ?>
 		</div>
 	</div>
 	<div class="callout_action <?php echo $call_align;?>">
-		<?php $this->draw_action($call_link, $target, $call_img, $call_btheme, $call_btext); ?>
+		<?php $this->draw_action( $call_link, $target, $call_img, $call_btheme, $call_btext ); ?>
 	</div>
-	
+
 </div>
 <?php else: ?>
 <div class="callout-area media fix <?php echo $styling_class;?>">
 	<div class="callout_action img <?php echo $call_align;?>">
-		<?php $this->draw_action($call_link, $target, $call_img, $call_btheme, $call_btext); ?>
+		<?php $this->draw_action( $call_link, $target, $call_img, $call_btheme, $call_btext ); ?>
 	</div>
 	<div class="callout_text bd">
 		<div class="callout_text-pad">
-			<?php $this->draw_text($call_title, $call_sub, $call_img); ?>
+			<?php $this->draw_text( $call_title, $call_sub, $call_img ); ?>
 		</div>
 	</div>
 </div>
-<?php endif; ?>
-<?php
-
+<?php endif;
 		} else
-			echo setup_section_notify($this, __('Set Callout page options to activate.', 'pagelines') );
-			
+			echo setup_section_notify( $this, __( 'Set Callout page options to activate.', 'pagelines' ) );
 	}
-	
-	function draw_action($call_link, $target, $call_img, $call_btheme, $call_btext){
+
+	function draw_action( $call_link, $target, $call_img, $call_btheme, $call_btext ){
 		if( $call_img )
-			printf('<div class="callout_image"><a %s href="%s" ><img src="%s" /></a></div>', $target, $call_link, $call_img);
-		else 
-			printf('<a %s class="btn btn-%s btn-large" href="%s">%s</a> ', $target, $call_btheme, $call_link, $call_btext);
-		
+			printf('<div class="callout_image"><a %s href="%s" ><img src="%s" /></a></div>', $target, $call_link, $call_img );
+		else
+			printf('<a %s class="btn btn-%s btn-large" href="%s">%s</a> ', $target, $call_btheme, $call_link, $call_btext );
+
 	}
-	
-	function draw_text($call_title, $call_sub, $call_img){
-		printf( '<h2 class="callout_head %s">%s</h2>', (!$call_img) ? 'noimage' : '', $call_title);
-		
-		if($call_sub)
-			printf( '<p class="callout_sub subhead %s">%s</p>', (!$call_img) ? 'noimage' : '', $call_sub);
+
+	function draw_text( $call_title, $call_sub, $call_img ){
+		printf( '<h2 class="callout_head %s">%s</h2>', ( ! $call_img ) ? 'noimage' : '', $call_title );
+
+		if( $call_sub )
+			printf( '<p class="callout_sub subhead %s">%s</p>', ( ! $call_img ) ? 'noimage' : '', $call_sub );
 	}
-	
 }
