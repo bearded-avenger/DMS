ca275104d6c6eb8075577bd5fd9623fccf27a8d6
Simon Prosser <simon@pagelines.com>
2013-03-14 22:51:51 +0000

    New LESS, new API, new DAY
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- editor/editor.color.php
+++ editor/editor.color.php
@@ -2,21 +2,24 @@
 
 
 class EditorColor{
-	
+
 	var $default_base = '#FFFFFF';
 	var $default_text = '#000000';
 	var $default_link = '#225E9B';
-	
+	var $background = '';
+
 	function __construct( ){
-		
-		
+
+		$this->background = pl_setting('page_background_image');
+
  		add_filter('pl_settings_array', array(&$this, 'add_settings'));
 		add_filter('pless_vars', array(&$this, 'add_less_vars'));
-		
+		if( $this->background );
+			add_action( 'wp_enqueue_scripts', array(&$this, 'background_fit'));
 	}
-	
+
 	function add_less_vars( $vars ){
-		
+
 		$base = ( pl_setting('bodybg') ) ? pl_setting('bodybg') : $this->default_base;
 		$text = ( pl_setting('text_primary') ) ? pl_setting('text_primary') : $this->default_text;
 		$link = ( pl_setting('linkcolor') ) ? pl_setting('linkcolor') : $this->default_link;
@@ -27,137 +30,135 @@ class EditorColor{
 		$vars['pl-link']				= $this->hash( $link );
 		$vars['pl-header']				= $this->hash( $hdrs );
 		$vars['pl-background']			= $this->background();
-		
 		return $vars;
 	}
-	
+
 	function background(){
-		
-		$fit = pl_setting('supersize_bg'); 
-		$image = pl_setting('page_background_image'); 
-		
+
+		$fit = pl_setting('supersize_bg');
+		$image = $this->background;
+
 		if($image && $fit){
-			add_action( 'wp_enqueue_scripts', array(&$this, 'background_fit'));
 			$background = $this->base;
 		}
 		elseif($image && !$fit)
-			$background = sprintf('%s url("%s") no-repeat', $this->base, $image); 
-		else 
+			$background = sprintf('%s url("%s") no-repeat', $this->base, $image);
+		else
 			$background = $this->base;
-			
+
 		return $background;
 	}
-	
+
 	function background_fit(){
-		wp_enqueue_script( 'pagelines-supersize' );			
+		wp_enqueue_script( 'pagelines-supersize' );
 		add_action('pl_scripts_on_ready', array(&$this, 'run_background_fit'), 20);
 	}
-	
+
 	function run_background_fit(){
 
-		$image = pl_setting('page_background_image'); 
+		$image = pl_setting('page_background_image');
 		?>
 		jQuery.supersized({ slides: [{ image : '<?php echo $image; ?>' }]})
 <?php
 	}
-	
-	
+
+
 	function hash( $color ){
-		
+
 		$clean = str_replace('#', '', $color);
 
 		return sprintf('#%s', $clean);
-		
+
 	}
 
-	
+
 	function add_settings( $settings ){
-		
+
 		$settings['color_control'] = array(
-			'name' 	=> 'Color <span class="spamp">&amp;</span> Style', 
+			'name' 	=> 'Color <span class="spamp">&amp;</span> Style',
 			'icon'	=> 'icon-tint',
 			'pos'	=> 3,
 			'opts' 	=> $this->options()
 		);
-		
+
 		return $settings;
 	}
-	
+
 	function options(){
-		
+
 		$settings = array(
 			array(
-				'key'		=> 'canvas_colors', 
+				'key'		=> 'canvas_colors',
 				'type' 		=> 'multi',
 				'title' 	=> __( 'Website Base Color', 'pagelines' ),
-				'help' 		=> __( 'The "base" color is used as your background and as a basis for calculating contrast values in elements (like hover effects, etc.. ) Use it as your default background color and refine using custom CSS/LESS or a theme.' ), 
+				'help' 		=> __( 'The "base" color is used as your background and as a basis for calculating contrast values in elements (like hover effects, etc.. ) Use it as your default background color and refine using custom CSS/LESS or a theme.' ),
 				'opts'		=> array(
-					array(	
+					array(
 						'key'			=> 'bodybg',
-						'type'			=> 'color',			
+						'type'			=> 'color',
 						'label' 		=> __( 'Base Color', 'pagelines' ),
 						'default'		=> $this->default_base
 					),
-				)		
+				)
 			),
 			array(
-				'key'		=> 'text_colors', 
+				'key'		=> 'text_colors',
 				'type' 		=> 'multi',
 				'label' 	=> __( 'Site Text Colors', 'pagelines' ),
 				'title' 	=> __( 'Site Text Colors', 'pagelines' ),
-				'help' 		=> __( 'Configure the basic text colors for your site', 'pagelines' ), 
+				'help' 		=> __( 'Configure the basic text colors for your site', 'pagelines' ),
 				'opts'		=> array(
-					array(	
+					array(
 						'key'			=> 'text_primary',
-						'type'			=> 'color',			
+						'type'			=> 'color',
 						'label' 		=> __( 'Main Text Color', 'pagelines' ),
 						'default'		=> $this->default_text
-						
+
 					),
-					array(	
+					array(
 						'key'			=> 'headercolor',
-						'type'			=> 'color',		
+						'type'			=> 'color',
 						'label' 		=> __( 'Text Headers Color', 'pagelines' ),
 						'default'		=> $this->default_text
 						),
-					array(		
+					array(
 						'key'			=> 'linkcolor',
 						'type'			=> 'color',
 						'label' 		=> __( 'Link Color', 'pagelines' ),
 						'default'		=> $this->default_link
 					)
-				)		
+				)
 			),
 			array(
-				'key'		=> 'background_style', 
+				'key'		=> 'background_style',
 				'type' 		=> 'multi',
-			
+
 				'title' 	=> __( 'Background Image', 'pagelines' ),
-				'help' 		=> __( '', 'pagelines' ), 
+				'help' 		=> __( '', 'pagelines' ),
 				'opts'		=> array(
-					array(	
+					array(
 						'key'			=> 'page_background_image',
-						'type'			=> 'image_upload',			
+						'type'			=> 'image_upload',
 						'label' 		=> __( 'Page Background Image', 'pagelines' ),
 						'default'		=> ''
-						
+
 					),
-					array(	
+					array(
 						'key'			=> 'supersize_bg',
-						'type'			=> 'check',		
+						'type'			=> 'check',
 						'label' 		=> __( 'Fit image to page?', 'pagelines' ),
 						'default'		=> true
 						),
-				)		
+				)
 			),
 
 		);
-	
-			
+
+
 		return $settings;
-		
+
 	}
-	
+
 }
 
 
--- editor/editor.less.php
+++ editor/editor.less.php
@@ -1,254 +1,42 @@
 <?php
 
-class EditorLess {
+class EditorLess extends EditorLessHandler {
+
+	var $pless;
+	var $lessfiles;
 
 	function __construct( PageLinesLess $pless ) {
 
-		global $wp_styles;
-
-		$this->wp_styles = $wp_styles;
-
-		// Dependancy Injection (^^)
 		$this->pless = $pless;
+		$this->lessfiles = $this->get_core_lessfiles();
 
+		if( $this->is_draft() )
+			$this->draft_init();
 	}
 
-	function enqueue_styles() {
+	/**
+	 *
+	 *  Display Draft Less.
+	 *
+	 *  @package PageLines Framework
+	 *  @since 3.0
+	 */
+	function pagelines_draft_render() {
 
-		return;
+		if( isset( $_GET['pagedraft'] ) ) {
 
-		// remove main compiles-css
-		add_action( 'wp_print_styles', array( &$this, 'dequeue_css' ), 12 );
+			header( 'Content-type: text/css' );
+			header( 'Expires: ' );
+			header( 'Cache-Control: max-age=604100, public' );
 
-		// add stylesheet/less to wp_enqueue_styles
-		add_filter( 'style_loader_tag', array( &$this, 'fix_less_styletag' ), 5, 2);
+			$core = $this->googlefont_replace( $this->get_draft_core() );
 
-		$this->create_file();
-		$this->enqueue_less();
-
-	}
-
-
-	function get_constants() {
-
-		$pless = new PageLinesLess;
-		$vars_array = $pless->constants;
-		$vars = '';
-		foreach($vars_array as $key => $value)
-			$vars .= sprintf('@%s:%s%s;%s', $key, " ", $value, "\n");
-
-		return $vars;
-	}
-
-
-
-	function dequeue_css() {
-
-		wp_deregister_style( 'pagelines-less' );
-	}
-
-	function get_core_less() {
-
-		$less = array( 'variables', 'mixins', 'colors' );
-		global $render_css;
-		return array_merge( $less, $render_css->get_core_lessfiles() );
-	}
-
-
-
-
-	function create_file() {
-
-		$less = $this->get_constants();
-
-		$core_files = $this->get_core_less();
-
-		foreach( $core_files as $k => $file ) {
-
-			$less .= pl_file_get_contents( trailingslashit( PL_CORE_LESS ) . $file . '.less' );
-
+			echo $this->minify( $core['compiled_core'] );
+			echo $this->minify( $core['compiled_sections'] );
+			echo $this->minify( $core['type'] );
+			echo $this->minify( $core['dynamic'] );
+			die();
 		}
-
-		$less .= $this->get_sections();
-
-		$this->write_css_file( $less );
 	}
 
-
-	function enqueue_less() {
-
-		wp_enqueue_style( 'editor-less', $this->get_css_dir( 'url' ) . '/editor.less' );
-	}
-
-	function fix_less_styletag( $tag, $handle ) {
-
-	    global $wp_styles;
-
-	    $match_pattern = '/\.less$/U';
-
-	    if ( preg_match( $match_pattern, $wp_styles->registered[$handle]->src ) ) {
-
-			$handle = $wp_styles->registered[$handle];
-
-	        $handle = $handle->handle;
-	        $media = $handle->args;
-	        $href = $handle->src;
-	        $rel = isset( $handle->extra['alt'] ) && $handle->extra['alt'] ? 'alternate stylesheet' : 'stylesheet';
-	        $title = isset( $handle->extra['title'] ) ? "title='" . esc_attr( $handle->extra['title'] ) . "'" : '';
-
-	        $tag = "<link rel='stylesheet/less' href='$href' type='text/css'>\n";
-	    }
-
-	    return $tag;
-	}
-
-	function write_css_file( $txt ){
-
-		global $wp_filesystem;
-
-		add_filter('request_filesystem_credentials', '__return_true' );
-
-		$method = '';
-		$url = 'themes.php?page=pagelines';
-
-		$folder = $this->get_css_dir( 'path' );
-		$file = 'editor.less';
-
-		if( !is_dir( $folder ) )
-			wp_mkdir_p( $folder );
-
-		include_once( ABSPATH . 'wp-admin/includes/file.php' );
-
-		if ( is_writable( $folder ) ){
-			$creds = request_filesystem_credentials($url, $method, false, false, null);
-			if ( ! WP_Filesystem($creds) )
-				return false;
-		}
-
-
-		if( is_object( $wp_filesystem ) )
-			$wp_filesystem->put_contents( trailingslashit( $folder ) . $file, $txt, FS_CHMOD_FILE);
-		else
-			return false;
-
-	}
-
-	function get_css_dir( $type = '' ) {
-
-		$folder = wp_upload_dir();
-
-		if( 'path' == $type )
-			return trailingslashit( $folder['basedir'] ) . 'pagelines';
-		else
-			return trailingslashit( $folder['baseurl'] ) . 'pagelines';
-	}
-
-	function get_sections() {
-
-		$out = '';
-		global $load_sections;
-		$available = $load_sections->pagelines_register_sections( true, true );
-
-		$disabled = get_option( 'pagelines_sections_disabled', array() );
-
-		/*
-		* Filter out disabled sections
-		*/
-		foreach( $disabled as $type => $data ){
-
-			if ( isset( $disabled[$type] ) ){
-
-				foreach( $data as $class => $state ){
-					unset( $available[$type][ $class ] );
-				}
-
-			}
-
-		}
-
-		/*
-		* We need to reorder the array so sections css is loaded in the right order.
-		* Core, then pagelines-sections, followed by anything else.
-		*/
-		$sections = array();
-
-		$sections['parent'] = $available['parent'];
-
-		unset( $available['parent'] );
-
-		$sections['child'] = (array) $available['child'];
-
-		unset( $available['child'] );
-
-		if ( is_array( $available ) )
-			$sections = array_merge( $sections, $available );
-
-		foreach( $sections as $t ) {
-			foreach( $t as $key => $data ) {
-				if ( $data['less'] && $data['loadme'] ) {
-					if ( is_file( $data['base_dir'] . '/style.less' ) )
-						$out .= pl_file_get_contents( $data['base_dir'] . '/style.less' );
-					elseif( is_file( $data['base_dir'] . '/color.less' ))
-						$out .= pl_file_get_contents( $data['base_dir'] . '/color.less' );
-				}
-			}
-		}
-
-		return apply_filters('pagelines_lesscode', $out);
-
-	}
-/*
-
-	function add_constants() {
-		printf( "<style id='pl-custom-less' type='text/less'>%s</style>\n",
-		$this->get_constants()
-		);
-	}
-
-	// experimenting with escaping the less variables...
-	function escape( $value ) {
-
-
-	return $value;
-
-		if( preg_match( '#"#', $value ) )
-			return '~' . rtrim( $value, '"' ) . '"';
-		else
-			return $value;
-	}
-
-	function enqueue_core_less() {
-
-		foreach ( $this->get_core_less() as $k => $file ) {
-
-			$id = $file;
-			$file 	= sprintf( '%s.less', $file );
-			$parent = sprintf( '%s/%s', PL_CORE_LESS, $file );
-			$parent_url = sprintf( '%s/%s', PL_CORE_LESS_URL, $file );
-			$child 	= sprintf( '%s/%s', PL_CHILD_LESS, $file );
-			$child_url = sprintf( '%s/%s', PL_CHILD_LESS_URL, $file );
-			if ( is_file( $child ) )
-				wp_enqueue_style( $id, $child_url );
-			else
-				wp_enqueue_style( $id, $parent_url );
-		}
-
-	}
-
-		function make_imports() {
-
-		$files = $this->get_core_less();
-		$out = '';
-
-		foreach ( $files as $k => $file ) {
-
-
-			$out .= sprintf( "<style id='pl-custom-less' type='text/less'>%s</style>\n",
-				sprintf( '%s@import "%s/%s";', "\n", PL_CORE_LESS_URL, $file )
-			);
-		}
-
-		echo $out;
-	}
-*/
 } // EditorLess
