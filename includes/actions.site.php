556e5e7775af573f037bff8a3b18c2604f93381f
Simon Prosser <simon@pagelines.com>
2013-04-24 19:46:18 +0100

    might as well register easing as well.
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- includes/actions.site.php
+++ includes/actions.site.php
@@ -129,7 +129,7 @@ function pagelines_register_js() {
 	wp_register_script( 'pagelines-resizer', PL_JS . '/script.resize.js', array( 'jquery' ), PL_CORE_VERSION, true );
 
 	wp_register_script( 'pagelines-viewport', PL_JS . '/script.viewport.js', array( 'jquery' ), PL_CORE_VERSION, true );
-
+	wp_register_script( 'pagelines-easing', PL_JS . '/script.easing.js', array( 'jquery' ), PL_CORE_VERSION, true );
 	wp_register_script( 'pagelines-common', PL_JS . '/pl.common.js', array( 'jquery' ), PL_CORE_VERSION, true );
 }
 
