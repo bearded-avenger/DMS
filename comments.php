d4ae31a7fd7974aa0cff225a0592a4a985e936ad
Andrew Powers <andrew@pagelines.com>
2013-05-27 23:29:34 -0700

    comments fix

--- comments.php
+++ comments.php
@@ -17,8 +17,13 @@
  * @version     2.1.1
  */
 
-if(!have_comments() && !comments_open())
+if(!have_comments() && !comments_open()){
+	
+	printf('<p class="nocomments">%s</p>', __('Comments are closed.', 'pagelines'));
 	return;
+	
+}
+	
 ?>
 <div id="comments" class="wp-comments">
 	<div class="wp-comments-pad">
@@ -38,27 +43,16 @@ if(!have_comments() && !comments_open())
 			printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'pagelines' ),
 			number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' );
 			?></h3>
+		<ol class="commentlist">
+			<?php wp_list_comments(array('avatar_size' => 65)); ?>
+		</ol>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+			<div class="navigation fix">
+				<div class="alignleft"><?php previous_comments_link( __( "<span class='meta-nav'>&larr;</span> Older Comments", 'pagelines' ) ); ?></div>
+				<div class="alignright"><?php next_comments_link( __( "Newer Comments <span class='meta-nav'>&rarr;</span>", 'pagelines' ) ); ?></div>
+			</div> <!-- .navigation -->
+		<?php endif; // check for comment navigation 
 
-			<ol class="commentlist">
-				<?php wp_list_comments(array('avatar_size' => 65)); ?>
-			</ol>
-			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-						<div class="navigation fix">
-							<div class="alignleft"><?php previous_comments_link( __( "<span class='meta-nav'>&larr;</span> Older Comments", 'pagelines' ) ); ?></div>
-							<div class="alignright"><?php next_comments_link( __( "Newer Comments <span class='meta-nav'>&rarr;</span>", 'pagelines' ) ); ?></div>
-						</div> <!-- .navigation -->
-			<?php endif; // check for comment navigation ?>
-
-<?php 	else : // or, if we don't have comments:
-
-			/* If there are no comments and comments are closed,
-			 * let's leave a little note, shall we?
-			 */
-			if ( ! comments_open() ) :
-		?>
-			<p class="nocomments"><?php _e('Comments are closed.', 'pagelines');?></p>
-		<?php endif; // end ! comments_open() ?>
-<?php
 		endif; // end have_comments()
 	comment_form(); ?>
 	</div>
