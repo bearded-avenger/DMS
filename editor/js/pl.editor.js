756b8cd0b239d2fa7998f538ab38651a7bc26d24
Andrew Powers <andrew@pagelines.com>
2013-06-28 15:40:40 -0700

    FIX bug i just made

--- editor/js/pl.editor.js
+++ editor/js/pl.editor.js
@@ -75,14 +75,14 @@
 			
 			// Show unload if state is changed from live, will be overridden if 
 			// the state changes or by other user actions.
-			if( $('#stateTool').data('show-unload') == 'yes' )
+		//	if( $('#stateTool').data('show-unload') == 'yes' )
 				//pl_show_unload()
 			
 			// Click event listener
 			$(".btn-toolbox").on("click.toolboxHandle", function(e) {
 
 				e.preventDefault()
-
+				
 
 				var btn = $(this)
 				, 	btnAction = btn.data('action')
